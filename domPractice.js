cc9926e02f048df5c2bbc090c9a4bfed76647908
Ashtonmey <ashcalebmeyer@gmail.com>
2023-01-03 16:57:49 +0200

    Completed DOM manipulation exercises

--- domPractice.js
+++ domPractice.js
@@ -1,5 +1,5 @@
 
-const container = document.querySelector('#container');
+const container = document.querySelector('#container'); // create a variable to store the selected element
 
 const p = document.createElement('p');
 p.classList.add('content');
@@ -8,10 +8,10 @@ p.setAttribute('style', 'color: red');
 
 container.appendChild(p);
 
-const h3 = document.createElement('h3');
-h3.classList.add('heading3');
-h3.textContent = 'I\'m a blue h3!';
-h3.setAttribute('style', 'color: blue');
+const h3 = document.createElement('h3'); // create variable for the created element
+h3.classList.add('heading3'); // give the element a class
+h3.textContent = 'I\'m a blue h3!'; // add text to the element
+h3.setAttribute('style', 'color: blue'); // style the element
 
 container.appendChild(h3);
 
@@ -28,5 +28,29 @@ p2.textContent = "ME TOO!";
 div.appendChild(p2);
 
 container.appendChild(div);
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+    alert("Hello World!")
+})
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', alertFunction);
+btn3.addEventListener('click', function(e) {
+    console.log(e.target.style.background = 'blue')
+});
+
+const btn4 = document.querySelector('#btn4')
+btn4.addEventListener('click', function(e) {
+    console.log(e.target.style.background = 'blue')
+});
+
+function alertFunction()
+{
+    alert ("This is the way to do it.")
+}
 
 
